e058cb67f11fe7d406562310a67a0655eb68041c
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-03-15 23:15:32 +0530

    adding fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,30 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-   variable: "--font-geist-sans",
-   subsets: ["latin"],
+const kelson = localFont({
+   src: [
+      {
+         path: "../font/KelsonSans-Normal.woff2",
+         weight: "400",
+         style: "normal",
+      },
+      {
+         path: "../font/KelsonSans-Bold.woff2",
+         weight: "700",
+         style: "normal",
+      },
+   ],
 });
-
-const geistMono = Geist_Mono({
-   variable: "--font-geist-mono",
-   subsets: ["latin"],
+const cervanttis = localFont({
+   src: [
+      {
+         path: "../font/Cervanttis.woff2",
+         weight: "400",
+         style: "normal",
+      },
+   ],
+   variable: "--font-cervanttis",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +40,7 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={`${kelson.className} ${cervanttis.variable} antialiased bg-norway-500`}
          >
             {children}
          </body>
