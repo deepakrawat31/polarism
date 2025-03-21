86df612a810de746e0e670307f329deb482079e2
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-03-21 23:27:32 +0530

    ui update

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,32 +1,36 @@
+import { Asterisk } from "@phosphor-icons/react/dist/ssr";
 import Link from "next/link";
 
 export default function Header() {
-   return (
-      <header className="flex items-center justify-between gap-4 p-6 sticky top-0 z-50 bg-neutral-900 border-b border-b-neutral-300/10 *:uppercase">
-         <Link
+  return (
+    <header className="sticky top-0 z-50 flex items-center justify-between gap-4 border-b border-b-neutral-300/10 bg-neutral-900 p-6 *:uppercase">
+      <Link
+        href={"/"}
+        className="text-neutral-300/40 transition-colors duration-300 hover:text-neutral-300"
+      >
+        home
+      </Link>
+      <ul className="flex items-center gap-4">
+        <li>
+          <Link
             href={"/"}
-            className="hover:text-neutral-300 transition-colors duration-300 text-neutral-300/40"
-         >
-            home
-         </Link>
-         <ul className="flex items-center gap-4">
-            <li>
-               <Link
-                  href={"/"}
-                  className="hover:text-neutral-300 transition-colors duration-300 text-neutral-300/40"
-               >
-                  take picture
-               </Link>
-            </li>
-            <li>
-               <Link
-                  href={"/"}
-                  className="hover:text-neutral-300 transition-colors duration-300 text-neutral-300/40"
-               >
-                  log in
-               </Link>
-            </li>
-         </ul>
-      </header>
-   );
+            className="text-neutral-300/40 transition-colors duration-300 hover:text-neutral-300"
+          >
+            take picture
+          </Link>
+        </li>
+        <li>
+          <Asterisk className="size-4 text-neutral-300/40" />
+        </li>
+        <li>
+          <Link
+            href={"/"}
+            className="text-neutral-300/40 transition-colors duration-300 hover:text-neutral-300"
+          >
+            log in
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import { ArrowUpRight } from "@phosphor-icons/react/dist/ssr";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-[40rem] flex-col items-center justify-center gap-8 p-4 md:p-8">
       <h1 className="font-ampunsuhu text-9xl">polarism</h1>
-      <p className="flex w-full max-w-3xl flex-col gap-8"></p>
-      <Link href={"/"}></Link>
+      <p className="uppercase">get goofy and click some memorable pictures.</p>
+      <Link
+        href={"/"}
+        className="flex items-center gap-2 uppercase *:rounded-full *:bg-neutral-300 *:text-neutral-900"
+      >
+        <span className="px-4 py-1.5">get your pose ready</span>
+        <span className="p-2">
+          <ArrowUpRight className="size-5" weight="light" />
+        </span>
+      </Link>
     </main>
   );
 }
