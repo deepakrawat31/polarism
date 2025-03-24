b3ca0a7e887f8c354bb11f2ebaca015184704358
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-03-24 22:35:37 +0530

    font change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,42 +4,42 @@ import Header from "./components/Header";
 import "./globals.css";
 
 const pitagon = localFont({
-   src: [
-      {
-         path: "../font/PitagonSansMono.woff2",
-         style: "normal",
-      },
-   ],
+  src: [
+    {
+      path: "../font/PitagonSansMono.woff2",
+      style: "normal",
+    },
+  ],
 });
-const ampunsuhu = localFont({
-   src: [
-      {
-         path: "../font/Ampunsuhu.woff2",
-         weight: "400",
-         style: "normal",
-      },
-   ],
-   variable: "--font-ampunsuhu",
+const cervanttis = localFont({
+  src: [
+    {
+      path: "../font/Cervanttis.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-cervanttis",
 });
 
 export const metadata: Metadata = {
-   title: "Polarism",
-   description: "Aesthetically pleasing single polaroid and multi set photos.",
+  title: "Polarism",
+  description: "Aesthetically pleasing single polaroid and multi set photos.",
 };
 
 export default function RootLayout({
-   children,
+  children,
 }: Readonly<{
-   children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-   return (
-      <html lang="en">
-         <body
-            className={`${pitagon.className} ${ampunsuhu.variable} antialiased bg-neutral-900 text-neutral-300`}
-         >
-            <Header />
-            {children}
-         </body>
-      </html>
-   );
+  return (
+    <html lang="en">
+      <body
+        className={`${pitagon.className} ${cervanttis.variable} bg-neutral-900 text-neutral-300 antialiased`}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-[40rem] flex-col items-center justify-center gap-8 p-4 md:p-8">
-      <h1 className="font-ampunsuhu text-9xl">polarism</h1>
-      <p className="uppercase">get your poses ready and</p>
-      <Link
-        href={"/"}
-        className="flex items-center gap-2 uppercase *:rounded-full *:bg-neutral-300 *:text-neutral-900"
-      >
-        <span className="px-4 py-1.5">start clicking.</span>
-        <span className="p-2">
-          <ArrowUpRight className="size-5" weight="light" />
-        </span>
+      <h1 className="font-cervanttis text-7xl text-amber-400 md:text-[10rem]">
+        polarism
+      </h1>
+      <p className="max-w-xl text-center text-neutral-300/40 uppercase">
+        a place to click some memorable pictures. goof up and start taking
+        pictures.
+      </p>
+      <Link href={"/"} className="flex items-center gap-2 uppercase">
+        <span className="">take picture</span>
+        <ArrowUpRight className="size-5" />
       </Link>
     </main>
   );
